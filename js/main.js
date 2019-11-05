30ec39420bb2667de0de7825bd2e49d6095c3df9
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-11-05 17:31:32 +0300

    try fix live link issues in my project

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// jquery for image toggle
+$(document).ready(function() {
+  $("#whatText").hide();
+  $("#whatText2").hide();
+  $("#whatText3").hide();
+  $(".imgtoggle01, .imgtoggle1").click(function() {
+    $("#whatText").toggle();
+    $(".imgtoggle01").toggle();
+  });
+  $(".imgtoggle02, .imgtoggle2").click(function() {
+    $("#whatText2").toggle();
+    $(".imgtoggle02").toggle();
+  });
+  $(".imgtoggle03, .imgtoggle3").click(function() {
+    $("#whatText3").toggle();
+    $(".imgtoggle03").toggle();
+  });
+});
+//hover effect on the portfolio section
+$(document).ready(function() {
+  $(".myportfolio").hover(
+    function() {
+      $(this)
+        .find(".myproject")
+        .show();
+    },
+    function() {
+      $(this)
+        .find(".myproject")
+        .hide();
+    }
+  );
+});
+
+//poup on submission
+$(document).ready(function() {
+  document.getElementById("form").addEventListener("submit", popUp);
+  function popUp() {
+    alert("Thanks Your message has been received.");
+  }
+});
